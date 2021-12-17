569c9aa55f76b7f902352a62b76a5d20772691f7
Dwinanto Saputra <dwinanto@grip-principle.com>
2021-12-17 13:41:58 +0700

    [LOG-2592] add completed supervisor screen

---
+++ router/warehouse/inbound/supervisor/completed.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import {Avatar, Text, Button, Input, Badge} from 'react-native-elements';
+import {View} from 'react-native';
+import Checkmark from '../../../../assets/icon/iconmonstr-check-mark-8mobile.svg';
+import {connect} from 'react-redux';
+import Mixins from '../../../../mixins';
+
+import OfflineMode from '../../../../component/linked/offlinemode';
+
+class Completed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bottomSheet: false,
+      isShowSignature: false,
+    };
+   
+  }
+
+
+
+  render(){
+    return (
+        <View style={{flex: 1, flexDirection:'column', backgroundColor: 'white', paddingHorizontal: 22,paddingVertical: 100}}>
+         <View style={{alignItems:'center', justifyContent: 'center',flexDirection: 'column'}}>
+             <Checkmark height="103" width="103" fill="#17B055"/>
+            <Text style={{...Mixins.h1,lineHeight: 36, color:'#17B055',marginVertical: 30}}>JOB COMPLETED</Text>
+          </View>
+          <View style={{alignSelf: 'center'}}>
+          <Button
+              containerStyle={{width: 300}}
+              buttonStyle={styles.navigationButton}
+              titleStyle={styles.deliveryText}
+              title="Back to Job List"
+              onPress={()=> this.props.navigation.navigate('ListSupervisor')}
+            />
+          </View>
+        </View>
+    );
+  }
+}
+
+const styles = {
+  sectionSheetButton: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+  deliveryText: {
+    ...Mixins.subtitle3,
+    lineHeight: 21,
+    color: '#ffffff',
+  },
+  navigationButton: {
+    backgroundColor: '#F07120',
+    borderRadius: 5,
+  },
+  sectionDividier: {
+    flexDirection: 'row',
+  },
+  buttonDivider: {
+    flex: 1,
+  },
+  sectionInput: {
+    flexDirection: 'column',
+    borderRadius: 13,
+    backgroundColor: 'transparent',
+    shadowColor: '#000',
+    marginBottom: 30,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+  inputHead: {
+    marginVertical: 12,
+    ...Mixins.h4,
+    lineHeight: 27,
+  },
+  sectionButtonGroup: {
+    flexDirection: 'row',
+  },
+  sectionContainer: {
+    flex: 1,
+    marginHorizontal: 5,
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  sectionText: {
+    textAlign: 'center',
+    width: 83,
+    ...Mixins.subtitle3,
+    lineHeight: 21,
+    color: '#6C6B6B',
+    marginVertical: 12,
+  },
+  containerInput: {
+    borderBottomColor: '#ABABAB',
+    borderBottomWidth: 1,
+    marginVertical: 0,
+    paddingVertical: 0,
+  },
+  inputStyle: {
+    ...Mixins.lineInputDefaultStyle,
+    ...Mixins.body1,
+    marginHorizontal: 0,
+    flexShrink: 1,
+    minHeight: 30,
+    lineHeight: 21,
+    fontWeight: '400',
+  },
+  labelStyle: {
+    ...Mixins.lineInputDefaultLabel,
+    ...Mixins.body1,
+    lineHeight: 14,
+    fontWeight: '400',
+  },
+  inputErrorStyle: {
+    ...Mixins.body2,
+    lineHeight: 14,
+    marginVertical: 0,
+  },
+  inputContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 13,
+    elevation: 8,
+    paddingHorizontal: 20,
+    paddingBottom: 5,
+    paddingTop: 0,
+  },
+  checkmark: {
+    position: 'absolute', 
+    bottom: 62, 
+    right: 16
+  },
+};
+function mapStateToProps(state) {
+  return {
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  
+    //toggleTodo: () => dispatch(toggleTodo(ownProps).todoId))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Completed);
+
